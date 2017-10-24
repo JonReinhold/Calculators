79f0f49658dae4c4af2cc0cf1d9172794c4f29c2
Jon Reinhold <jon.reinhold15@gmail.com>
2017-10-23 21:29:46 -0500

    Update CalcPy.py

--- CalcPy.py
+++ CalcPy.py
@@ -5,6 +5,7 @@ x = int(sys.argv[1])
 y = int(sys.argv[2])
 z = sys.argv[3]
 
+#
 def Calculator(a,b,c):
 	if z == "*":
 		return (x*y)
@@ -15,7 +16,7 @@ def Calculator(a,b,c):
 	elif z == "-":
 		return (x-y)
 	else:
-		return "Invalid input."
+		return "Invalid input"
 
 print(Calculator(x,z,y))
 	
