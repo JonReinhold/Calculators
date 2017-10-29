c64e93c53f4747c0fe88ec46f4c40030121480f2
Jon Reinhold <jon.reinhold15@gmail.com>
2017-10-29 17:48:40 -0500

    Create Calc.c

---
+++ Calc.c
@@ -0,0 +1,35 @@
+# include <stdio.h>
+int main() {
+
+    double firstNumber;
+	double secondNumber;
+    char operator;
+
+    printf("Which operation do you want to do: ");
+    scanf("%c", &operator);
+
+    printf("Numbers: ");
+    scanf("%lf %lf",&firstNumber, &secondNumber);
+
+   
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber + secondNumber);
+            break;
+
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber - secondNumber);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber * secondNumber);
+            break;
+
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
+            break;
+    }
+    
+    return 0;
+}
