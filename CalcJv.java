d1838b1e5896318f7ff34684d953c45f242ebd22
Jon Reinhold <jon.reinhold15@gmail.com>
2017-10-28 21:24:09 -0500

    Update CalcJv.java
    spacing...

--- CalcJv.java
+++ CalcJv.java
@@ -5,34 +5,34 @@ import static java.lang.System.out;
 public class CalcJv {
     public static void main(String[] args) {
     double arg1;
-		double arg2;
-        String action;
-        Scanner scannerObject = new Scanner(System.in);
+    double arg2;
+    String action;
+    Scanner scannerObject = new Scanner(System.in);
 
-        System.out.println("First number: ");
-        arg1 = scannerObject. nextDouble();
-        System.out.println("Second number: ");
-        arg2 = scannerObject. nextDouble();
-        Scanner op = new Scanner(System.in);
-        System.out.println("-,+,*,/ ?");
-        action = op.next();
+  System.out.println("First number: ");
+  arg1 = scannerObject. nextDouble();
+  System.out.println("Second number: ");
+  arg2 = scannerObject. nextDouble();
+  Scanner op = new Scanner(System.in);
+  System.out.println("-,+,*,/ ?");
+  action = op.next();
 
-        switch (action)  {
-        case "+":
-            System.out.println("Your answer is " + (arg1 + arg2));
-            break;
+  switch (action)  {
+  case "+":
+      System.out.println("Your answer is " + (arg1 + arg2));
+      break;
 
-        case "-":
-            System.out.println("Your answer is " + (arg1 - arg2));
-            break;
+  case "-":
+      System.out.println("Your answer is " + (arg1 - arg2));
+      break;
 
-        case "/":
-            System.out.println("Your answer is " + (arg1 / arg2));
-            break;
+  case "/":
+      System.out.println("Your answer is " + (arg1 / arg2));
+      break;
 
-        case "*":
-            System.out.println("Your asnwer is " + (arg1 * arg2));
-            break;
+  case "*":
+      System.out.println("Your asnwer is " + (arg1 * arg2));
+      break;
 
 
         }
